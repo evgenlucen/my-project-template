b7abe0a3f3e759514dff1d531577e3c0e3a4aa1a
Evgeniy Lutsenko <e.lutsenko@ipoteka.center>
2023-10-04 22:52:35 +0300

    Добавлен монолог, добавлен rabbitmq

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
